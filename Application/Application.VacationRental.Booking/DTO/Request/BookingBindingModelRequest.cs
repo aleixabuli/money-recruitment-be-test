a8849cb0b4d10db3fa53dcb6904df4de208b7e79
aleixabuli <aleixabuli@gmail.com>
2022-10-17 19:18:11 +0200

    Add required attributes to the requests

--- Application/Application.VacationRental.Booking/DTO/Request/BookingBindingModelRequest.cs
+++ Application/Application.VacationRental.Booking/DTO/Request/BookingBindingModelRequest.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.VacationRental.Booking.DTO.Request
 {
     public class BookingBindingModelRequest
     {
+        [Required]
         public int RentalId { get; set; }
 
+        [Required]
         public DateTime Start
         {
             get => _startIgnoreTime;
             set => _startIgnoreTime = value.Date;
         }
 
+
         private DateTime _startIgnoreTime;
+
+        [Required]
         public int Nights { get; set; }
+
+        [Required]
         public int Unit { get; set; }
     }
 }
--- Application/Application.VacationRental.Booking/DTO/Request/RentalBindingModelRequest.cs
+++ Application/Application.VacationRental.Booking/DTO/Request/RentalBindingModelRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace Application.VacationRental.Booking.DTO.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.VacationRental.Booking.DTO.Request
 {
     public class RentalBindingModelRequest
     {
+        [Required]
         public int Units { get; set; }
+
         public int PreparationTimeInDays { get; set; }
     }
 }
